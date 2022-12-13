b41ca76470599e58e860659a8e0dd617675b98f4
Denis B <denis.baudot@gmail.com>
2022-12-13 11:19:54 +0100

    add types and export get client, remove home controller form demo

--- src/controllers/page.ts
+++ src/controllers/page.ts
@@ -1,40 +1,49 @@
-import {RequestHandler} from "express";
-import {appUrl} from "../config/config";
-import {getClient} from './demo'
+import { RequestHandler, Request, Response } from "express";
+import { appUrl } from "../config/config";
+import { getClient } from "./demo";
 
+export const homeController: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  let userEmail = null;
+  if (req.session.access_token) {
+    const client = await getClient();
+    const userinfo = await client.userinfo(req.session.access_token);
+    userEmail = userinfo.preferred_username || userinfo.sub;
+  }
 
-
-export const homeController: RequestHandler = async (req, res) => {
-    let userEmail = null;
-    if (req.session.access_token) {
-        const client = await getClient();
-        const userinfo = await client.userinfo(req.session.access_token);
-        userEmail = userinfo.preferred_username || userinfo.sub
-    }
-
-    return res.render('home', {
-        loginUrl: appUrl + '/login',
-        emailDefault : process.env.EMAIL_DEFAULT || "aaa.bbb@beta.gouv.fr",
-        userEmail
-    });
+  return res.render("home", {
+    loginUrl: appUrl + "/login",
+    emailDefault: process.env.EMAIL_DEFAULT || "aaa.bbb@beta.gouv.fr",
+    userEmail,
+  });
 };
 
-export const fonctionnementController: RequestHandler = async (req, res) => {
-
-    return res.render('fonctionnement', {});
+export const fonctionnementController: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  return res.render("fonctionnement", {});
 };
 
-export const ressourcesController: RequestHandler = async (req, res) => {
-
-    return res.render('ressources', {});
+export const ressourcesController: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  return res.render("ressources", {});
 };
 
-export const documentationController: RequestHandler = async (req, res) => {
-
-    return res.render('documentation', {});
+export const documentationController: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  return res.render("documentation", {});
 };
 
-export const statistiquesController: RequestHandler = async (req, res) => {
-
-    return res.render('statistiques', {});
+export const statistiquesController: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  return res.render("statistiques", {});
 };
